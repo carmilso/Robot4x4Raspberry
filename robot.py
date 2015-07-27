20982f841df91acd32709ef286562806cf3749b5
Carlos Millán Soler <carmilso@inf.upv.es>
2015-07-27 18:57:19 +0200

    Robot test

--- robot.py
+++ robot.py
@@ -3,6 +3,14 @@
 
 import driversL298N as drivers
 
+
 def main():
-    robot = Robot(17, 27, 10, 9)
-    
+    robot = drivers.Robot(17, 27, 10, 9, 1)
+
+    robot.fordwardRobot(3)
+    robot.rightRobot(2)
+    robot.backwardRobot(3)
+
+
+if __name__ == '__main__':
+    main()
