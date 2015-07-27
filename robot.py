0843f7b522da4e5fecc1f2b64cd25ffac4890306
Raspberry <karlospv94@gmail.com>
2015-07-27 19:02:28 +0200

    Robot test

--- robot.py
+++ robot.py
@@ -7,9 +7,10 @@ import driversL298N as drivers
 def main():
     robot = drivers.Robot(17, 27, 10, 9, 1)
 
-    robot.fordwardRobot(3)
-    robot.rightRobot(2)
-    robot.backwardRobot(3)
+    robot.fordwardRobot(7)
+    robot.rightRobot(5)
+    robot.leftRobot(5)
+    robot.backwardRobot(7)
 
 
 if __name__ == '__main__':
