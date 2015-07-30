e5c96a4da0e899c42a28aedb05423b90bef017f5
Carlos Millán Soler <carmilso@inf.upv.es>
2015-07-30 21:31:45 +0200

    Prueba login

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use('/login/css', express.static(path.join(__dirname, 'login/css')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+var verifyCodes = {};
+
 
 /***********************************************************************/
 app.get('/', function(req, res) {
@@ -25,21 +27,27 @@ app.get('/register', function(req, res) {
 	res.sendFile(path.join(__dirname+'/login/register.html'));
 });
 
-app.post('/verify', function(req, res, next) {
+app.post('/verify', function(req, res) {
 	console.log('User: ' + req.body.userR);
 	console.log('Password: ' + req.body.passwordR);
 
 	var code = Math.random().toString(36).substring(2, 9);
 	console.log('Verification code: ' + code);
 
-	res.sendFile(path.join(__dirname+'/login/verify.html'));
-	return next();
-}), function(req, res) {
-	console.log('------->' + req.query.codeAdmin);
-}
+	verifyCodes[req.body.userR] = code;
+
+	res.render(path.join(__dirname+'/login/verify.ejs'), {
+		userR: req.body.userR
+	});
+});
 
 app.get('/validate', function(req, res) {
-
+	if (req.query.adminCode == verifyCodes[re.query.userR]){
+		console.log("Same code!");
+	}
+	else {
+		console.log("Not same code...");
+	}
 });
 /***********************************************************************/
 
