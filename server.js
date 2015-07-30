c80092ec22866718502a7f6946df33578bd73f62
Raspberry <karlospv94@gmail.com>
2015-07-30 11:37:39 +0200

    Added dynamically function to html

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ var express = require('express');
 var path    = require('path');
 var app     = express();
 
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.render(path.join(__dirname+'/index'), {
+		title: 'Prueba',
+		text: 'Hi! This is a prove to view the new html form.'
+	});
 });
 
-app.listen(3000, function() {
+var server = app.listen(3000, function() {
 	console.log('Server started');
 });
+
+process.on('SIGINT', function() {
+	server.close();
+	console.log('Server disconnected.');
+	process.exit(0);
+})
