a6947500100b4aaeed893d9fc5d68916f660959f
Carlos Millán Soler <carmilso@inf.upv.es>
2015-07-30 21:08:29 +0200

    Prueba login

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var passport		= require('passport');
 var bodyParser		= require('body-parser');
 var LocalStrategy	= require('passport-local').Strategy;
 
-var app = express();
 
+var app = express();
 
 app.set('view engine', 'ejs');
 
@@ -13,15 +13,8 @@ app.use('/login/css', express.static(path.join(__dirname, 'login/css')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-/*app.get('/', function(req, res) {
-	console.log('Connected: ' + req.connection.remoteAddress);
 
-	res.render(path.join(__dirname+'/index'), {
-		title: 'Testing ExpressJS',
-		text: 'Hi! This is a prove to view the new html form.'
-	});
-});*/
-
+/***********************************************************************/
 app.get('/', function(req, res) {
 	console.log('Connected: ' + req.connection.remoteAddress);
 
@@ -36,18 +29,21 @@ app.post('/verify', function(req, res) {
 	console.log('User: ' + req.body.userR);
 	console.log('Password: ' + req.body.passwordR);
 
-	res.sendFile(path.join(__dirname+'/login/verify.html'));
+	var code = Math.random().toString(36).substring(2, 9);
+	console.log('Verification code: ' + code);
+
+	res.sendFile(path.join(__dirname+'/login/verify.html'), function(req, res) {
+		console.log(req.query['codeAdmin']);
+	});
+
+
 });
 
 app.get('/validate', function(req, res) {
-	var code = Math.random().toString(36).substring(2, 9);
-	console.log('Verification code: ' + code);
-});
 
-app.get('/request', function(req, res) {
-	var val = req.query['value'];
-	res.send('Value introduced: ' + val);
 });
+/***********************************************************************/
+
 
 var server = app.listen(3000, function() {
 	console.log('Server started');
