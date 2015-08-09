f3d5c4bc7286ae3c9f9a3de23dba40742079263f
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-09 21:35:54 +0200

    Passport implementation

--- server.js
+++ server.js
@@ -26,7 +26,30 @@ var db = mysql.createConnection({
 	database: dataBaseInfo.database
 });
 
-require('./config/passport')(passport);
+passport.use(new LocalStrategy ({
+	usernameField: 'login__username',
+	passwordField: 'login__password',
+	passReqToCallback: true
+},
+function(req, username, password, done) {
+	findUser(username, password, function(err, user) {
+		if (err) return done(err);
+		if (!user) return done(null, false, req.flash('loginMessage', 'Incorrect username or password'));
+		if (user){
+			console.log('User received: ' + user);
+			return done(null, user);
+		}
+	});
+}
+));
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	done(null, user);
+});
 
 app.use(session({
 	secret: 'cat sleeping',
