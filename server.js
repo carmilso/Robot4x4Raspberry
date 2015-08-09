dc23a54d6d2c0a83c99d29357404e8b23242b53c
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-09 17:45:20 +0200

    Small modifications

--- server.js
+++ server.js
@@ -33,7 +33,10 @@ function(username, password, done) {
 	findUser(username, password, function(err, result) {
 		if (err) return done(err);
 		if (!result) return done(null, false, { message: 'Incorrect username or password' });
-		if (result) return done(null, result[0]);
+		if (result){
+			console.log('Result[0] -> ' + result[0]);
+			return done(null, result[0]);
+		}
 	});
 }
 ));
