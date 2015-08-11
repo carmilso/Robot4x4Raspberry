c6b351f69e836cbc98bd04adb1be8e55a773dc10
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-11 20:38:13 +0200

    Testing ajax

--- server.js
+++ server.js
@@ -98,9 +98,8 @@ function signUp(username, password, ip, callback) {
 
 
 /***********************************************************************/
-app.post('/communication', function(req, res) {
+app.post('/validateCode', function(req, res) {
 	var data = JSON.parse(JSON.stringify(req.body));
-	console.log(data);
 	console.log('Received: ' + data.code);
 	res.send('okay');
 });
