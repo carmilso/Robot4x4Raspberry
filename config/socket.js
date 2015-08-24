9f9c6a57cc04ab6805e9628c4d87cc5c2f21895e
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-24 19:23:33 +0200

    Developing socket.io

--- config/socket.js
+++ config/socket.js
@@ -1,9 +1,42 @@
 module.exports = function(socket) {
 
-	socket.on('connection', function(client) {
-        	console.log('[SOCKET] User connected');
-		client.on('disconnect', function() {
+	var usersConnected = 0;
+	var robotStates = ['stop', 'up', 'down', 'left', 'right'];
+	var robotState = robotStates[0];
+
+	socket.on('connection', function(user) {
+        console.log('[SOCKET] User connected');
+		usersConnected++;
+
+		socket.emit('usersConnected', {int: usersConnected});
+
+		var state = actualState(robotState);
+
+		user.emit('actualState', {html: state});
+
+		user.on('disconnect', function() {
 			console.log('[SOCKET] User disconnected');
+			usersConnected--;
+			socket.emit('usersConnected', {int: usersConnected});
 		});
+
 	});
 };
+
+function actualState(robotState) {
+	if (robotState == 'stop')
+		return '<h3>Stopped</h3>'
+
+	var angle = '';
+
+	else {
+		if (robotState == 'down')
+			angle = 180;
+		else if (robotState == 'left')
+			angle = 270;
+		else if (robotState == 'right')
+			angle = 90;
+	}
+
+	return '<span class="arrow-success-large" data-angle="' + angle + '" id="arrowState"></span>'
+}
