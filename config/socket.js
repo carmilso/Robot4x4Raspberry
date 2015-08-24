0de893751cd4a256c84c17b9c0966be8028e7927
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-24 20:47:42 +0200

    Developing socket.io

--- config/socket.js
+++ config/socket.js
@@ -14,6 +14,7 @@ module.exports = function(socket) {
 		user.emit('actualState', state);
 
 		user.on('arrow', function(state) {
+			console.log('Received: ' + state);
 			var aState = actualState(state);
 			socket.emit('actualState', aState);
 		});
