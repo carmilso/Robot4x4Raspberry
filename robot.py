615aed0c308930d2df5b0cafd3fa262972496555
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-25 20:50:31 +0200

    Implemented robot control

--- robot.py
+++ robot.py
@@ -10,11 +10,10 @@ from config import driversL298N as drivers
 def main():
     robot = drivers.Robot(17, 27, 10, 9, 0)
 
-    operations = {'up': robot.fordwardRobot(), 'down': robot.backwardRobot(), 'right': robot.rightRobot(), 'left': robot.leftRobot(), 'stop': robot.stopRobot()}
+    #operations = {'up': robot.fordwardRobot(), 'down': robot.backwardRobot(), 'right': robot.rightRobot(), 'left': robot.leftRobot(), 'stop': robot.stopRobot()}
 
     while True:
         order = raw_input()
-        print order, len(order)
 
         if order == 'close': break
         elif order == 'up': robot.fordwardRobot()
