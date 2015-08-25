032ce8febcce6b1ab201b6819a2e01941d139b55
Carlos Millán Soler <carmilso@inf.upv.es>
2015-08-25 20:44:23 +0200

    Implemented robot control

--- config/socket.js
+++ config/socket.js
@@ -22,7 +22,7 @@ module.exports = function(socket) {
 
 		user.on('arrow', function(state) {
 			var aState = getAngle(state);
-			robot.send(state + '\n');
+			robot.send(state);
 			socket.emit('actualState', aState);
 		});
 
